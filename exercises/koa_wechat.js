c274f390fc8832ee16f132f34778d101ac370499
haidao17 <yiwenqi1989@gmail.com>
2016-07-19 17:55:50 +0800

    add koa_wechat

---
+++ exercises/koa_wechat.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var Koa = require('koa');
+var sha1 = require('sha1');
+
+var config = {
+  wechat: {
+    appId:'wx6c599e405948d5f2',
+    appsecret:'2bd38a9144e7be2aeaddda45dce1e3ba',
+    token:'testwechat'
+  }
+}
+
+var app = new Koa();
+
+app.use(function *(next) {
+  console.log(this.query);
+
+  var token = config.wechat.token;
+  var signature = this.query.signature;
+  var nonce = this.query.nonce;
+  var timestamp = this.query.timestamp;
+  var echostr = this.query.echostr;
+  var str = [token,timestamp,nonce].sort().join('');
+  var sha = sha1(str);
+
+  if (sha === signature) {
+    this.body = echostr + '';
+  }else {
+    this.body = 'wrong';
+  }
+});
+
+app.listen(80);
+console.log('Listening: 80');
